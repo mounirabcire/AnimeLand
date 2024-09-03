eaa6f3ed119aa93044bca425342e542f7249fb50
mounirabcire <mounirwebdevcode@gmail.com>
2024-09-03 22:30:08 +0100

    fixed errors

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useState } from "react";
-import TopBattlesList from "../components/topBattlesList";
+import TopBattlesList from "./../components/TopBattlesList";
 
 const battles = [
     {
